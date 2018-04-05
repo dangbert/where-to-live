cb2db84efa11bfddcc4a206d781d25832ebba8f2
Daniel Engbert <end1@umbc.edu>
2018-04-05 00:21:03 -0400

    Created a quick webpage with a hamburger button for a search options menu and added the google maps api. Created search.php which returns a test string for now.

--- home.php
+++ home.php
@@ -12,14 +12,46 @@
 
     <body>
         <?php echo "<p>Hello world<p>"; ?>
-        <button type="button" id="example-button" class="btn btn-primary">Boostrap Example Button</button>
+        <button type="button" id="search-button" class="btn btn-primary">Search</button>
+
+        <!-- nav bar which will actually have search paramaters -->
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <h3>Search Options:</h3>
+            <div class="search-option">
+                <!-- Rounded switch -->
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider round"></span>
+                </label>
+                Public Schools
+            </div>
+            <div class="search-option">
+                <!-- Rounded switch -->
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider round"></span>
+                </label>
+                Public Transportation
+            </div>
+
+        </div>
+
+        <!-- hamburger button to open navbar-->
+        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Search Options</span>
+
+        <!--where the map will be rendered-->  
+        <center><div id="map"></div></center>
 
 
+        <!-- scripts to include: -->
         <!-- jquery -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
         <!-- bootstrap -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <!-- our javascript -->
         <script src="static/script.js"></script>
+        <!-- import google maps api (using my api key) -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIWdpUpiQnAIxMvVywRq7aVMDqNeXNJGo&callback=initMap&libraries=places" async defer></script>
     </body>
 </html>
--- static/script.js
+++ static/script.js
@@ -2,6 +2,52 @@ $(document).ready(function() {
     console.log("jquery ready");
 });
 
-$("#example-button").on("click", function() {
-    alert("you clicked the button");
+$("#search-button").on("click", function() {
+    // make a POST request to our search script
+    $.ajax({
+        type: "POST",
+        url: "/search.php",
+        data: {
+            "name": "code fury!"
+        },
+        success: function(resp) {
+            alert(resp);
+        },
+        error: function(resp) {
+            console.log(resp);
+        }
+    });
 });
+
+$("#close-nav").on("click", function() {
+    closeNav();
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// callback when google maps api loads
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.09024, lng: -100.712891}, //initially centered in the middle of the US, quickly replaced with current location
+        zoom: 4
+//        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    
+    
+    // attempt to get user location with W3C Geolocation (Preferred). see: tinyurl.com/gmproj3
+    var initialLocation;
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+            map.setZoom(11);
+        });
+    }
+}
