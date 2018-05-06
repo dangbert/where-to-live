7ecd7668206afcf03dc8e6fe6560719d84895b5b
Rushmie Kulkarni <rushmie@gmail.com>
2018-05-06 08:07:58 -0400

    more testing functions

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pymysql
 
 #https://stackoverflow.com/questions/23226074/simulating-ajax-post-call-using-python-requests
 #https://stackoverflow.com/questions/31824222/python-request-post-to-php-server
@@ -7,10 +8,35 @@ url = 'http://localhost/code_fury/controllers/search.php'
 data = {"schools":{"enabled":False,"value":0},"transportation":{"enabled":False,"value":0},"crime":{"enabled":False,"value":0},"recreation":{"enabled":True,"value":{"has_biking":True,"has_climbing":True,"has_camping":True,"has_hiking":True,"has_hunting":True,"has_wilderness":True,"has_swimming":True}},"climate":{"enabled":False,"value":{"temperature":0,"precipitation":0,"snowfall":0}},"healthcare":{"enabled":False,"value":0},"commute":{"enabled":False,"value":60}}
 #headers = {'User-Agent': "test.py", 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 
-#headers = requests.utils.default_headers()
-headers = {'content-type': 'application/json'} 
+headers = requests.utils.default_headers()
+#headers = {'content-type': 'application/json'} 
 
 s = requests.Session()
 resp = s.post(url, data = json.dumps(data), headers = headers)
 
 print(resp.text)
+
+def main():
+	showCounties()
+
+def getConnection():
+	return pymysql.connect(host='localhost',
+		user='code_fury',
+		password='devpassword',
+		db='code_fury')
+		
+		
+def showCounties():
+	connection = getConnection()
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT * from counties limit 10;"
+			cursor.execute(sql)
+			result = cursor.fetchone()
+			print(result)
+ 
+	finally:
+		connection.close()
+
+if __name__ == "__main__":
+	main()
