c00888855efea1fa40c06a4fa54c0fb28d5e07f6
Rushmie Kulkarni <rushmie@gmail.com>
2018-05-06 01:48:59 -0400

    more test stuff

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 https://stackoverflow.com/questions/23226074/simulating-ajax-post-call-using-python-requests
+https://stackoverflow.com/questions/31824222/python-request-post-to-php-server
 
 url = 'http://localhost/code_fury/controllers/search.php'
 data = {"schools":{"enabled":False,"value":0},"transportation":{"enabled":False,"value":0},"crime":{"enabled":False,"value":0},"recreation":{"enabled":True,"value":{"has_biking":True,"has_climbing":True,"has_camping":True,"has_hiking":True,"has_hunting":True,"has_wilderness":True,"has_swimming":True}},"climate":{"enabled":False,"value":{"temperature":0,"precipitation":0,"snowfall":0}},"healthcare":{"enabled":False,"value":0},"commute":{"enabled":False,"value":60}}
