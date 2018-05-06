b077afedfc05910be56420d2d55dfd19a60c4445
Rushmie Kulkarni <rushmie@gmail.com>
2018-05-06 01:45:22 -0400

    messing around with starting tests

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+https://stackoverflow.com/questions/23226074/simulating-ajax-post-call-using-python-requests
+
+url = 'http://localhost/code_fury/controllers/search.php'
+data = {"schools":{"enabled":False,"value":0},"transportation":{"enabled":False,"value":0},"crime":{"enabled":False,"value":0},"recreation":{"enabled":True,"value":{"has_biking":True,"has_climbing":True,"has_camping":True,"has_hiking":True,"has_hunting":True,"has_wilderness":True,"has_swimming":True}},"climate":{"enabled":False,"value":{"temperature":0,"precipitation":0,"snowfall":0}},"healthcare":{"enabled":False,"value":0},"commute":{"enabled":False,"value":60}}
+headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+
+s = requests.Session()
+resp = s.post(url, data = data, headers = headers)
+
+print(resp.text)
