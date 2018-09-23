8f2ee9bca1da6a19f9498627d27260ebe1c0c6c3
Daniel Engbert <end1@umbc.edu>
2018-09-23 19:36:26 -0400

    Small tweaks.

--- views/index.php
+++ views/index.php
@@ -1,6 +1,6 @@
 <?php
-    define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/');
+    //define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/');
+    define('ROOT_DIR', '..');
 
-
-    include ROOT_DIR . 'views/home.php';
+    include ROOT_DIR . '/views/home.html';
 ?>
